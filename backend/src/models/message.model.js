f0928eb87d52857d5216c2314851a53612c2ae45
AKhshAT10 <akhshat.sharma25@gmail.com>
2025-09-14 19:46:52 +0530

    created album,messages,songs,user models

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import { type } from 'os';
+
+const messageSchema = new mongoose.Schema({
+    senderId:{
+        type:String,
+        required:true
+    },
+    receiverId:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+},{timestamps:true});
+
+export const Message = mongoose.model("Message",messageSchema);
